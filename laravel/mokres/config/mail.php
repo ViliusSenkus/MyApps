e5e665ae144b356ed9b0e832f1d450c77b92942e
Vilius Senkus <vilius.s@outlook.com>
2023-03-18 21:22:27 +0200

    Config/mail and session files configured, Config/database file - not needed DB is put under comments

--- laravel/mokres/config/mail.php
+++ laravel/mokres/config/mail.php
@@ -98,8 +98,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'MokRes@info.lt'),
+        'name' => env('MAIL_FROM_NAME', 'MokRes Informacija'),
     ],
 
     /*
