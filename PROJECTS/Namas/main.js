94076c8d0f262c09c0671d1b0d7c7961ffa50aa4
ViliusSenkus <vilius.s@outlook.com>
2023-08-07 16:06:43 +0300

    biski lengvam prisiminimui

---
+++ PROJECTS/Namas/main.js
@@ -0,0 +1,7 @@
+document.write("biski teksto cia");
+
+import PurchasesData from "./model/purchasesData";
+
+let purchasesData = new PurchasesData("vardas");
+
+purchasesData.showName;
---
+++ PROJECTS/Namas/model/purchasesData.js
@@ -0,0 +1,12 @@
+class PurchasesData {
+
+      constructor (name){
+            name = this.name;
+      }
+
+      showName(){
+            document.write(this.name);
+      }
+}
+
+export default PurchasesData;
