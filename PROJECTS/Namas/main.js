c3f1ee04d3e8e6729c37502813cd6029d22bbfb3
ViliusSenkus <vilius.s@outlook.com>
2023-08-15 21:34:50 +0300

    .

--- PROJECTS/Namas/main.js
+++ PROJECTS/Namas/main.js
@@ -5,4 +5,6 @@ let rows = table.querySelectorAll("tr");
 let newRow = `<tr><td>2</td><td></td>${item.name}<td>425.73EUR</td><td>2022-07-18</td><td>Lemora</td></tr>`
 // table.append.newRow ...........
 
+let purchase = new Purchase();
 console.log(item.get());
+console.log(purchase.get());
