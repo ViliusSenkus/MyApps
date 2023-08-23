188066da1ce9dc85ba9de2f6e31d7de719d5c9d6
ViliusSenkus <vilius.s@outlook.com>
2023-08-23 21:55:15 +0300

    Few columns and comment added to purchases table

--- PROJECTS/Namas/backend/database/migrations/2023_08_22_193345_create_purchases_table.php
+++ PROJECTS/Namas/backend/database/migrations/2023_08_22_193345_create_purchases_table.php
@@ -18,15 +18,16 @@ return new class extends Migration
             $table->id();
             $table->date('purchase_date');
             $table->string('purchase_place', 255);
-            $table->float('total_payed', 8, 2)->unsigned();
-            $table->json('list_of_purchases'); 
+            // $table->float('total_payed', 8, 2)->unsigned(); //must be counted from exact_product or exact_service included in this purchase
+            $table->json('list_of_exact_purchases_ids'); 
                 //json string should consist product or service id, detailed description, purchased ammount, price, discount, etc...
 
                 //further strings (documents) require a more detailed analysis of necessity
-            $table->string('purchase_document', 255)->nullable();
-            $table->string('prepay_document', 255)->nullable();
-            $table->string('final_payment_document', 255)->nullable();
-            $table->string('offer_document', 255)->nullable();
+            $table->string('purchase_doc', 255)->nullable();
+            $table->string('prepay_doc', 255)->nullable();
+            $table->string('final_payment_doc', 255)->nullable();
+            $table->string('offer_doc', 255)->nullable();
+            $table->string('other_doc', 255)->nullable();
 
             $table->string('payment_method', 255); //cash, card, bank transfer ...
             
