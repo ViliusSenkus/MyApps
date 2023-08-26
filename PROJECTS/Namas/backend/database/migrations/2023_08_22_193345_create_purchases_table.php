a6206c07ac5ba67c02a7d87d5c2913b75e7d5e11
ViliusSenkus <vilius.s@outlook.com>
2023-08-26 15:25:46 +0300

    some info added to 'purchases table' migration

--- PROJECTS/Namas/backend/database/migrations/2023_08_22_193345_create_purchases_table.php
+++ PROJECTS/Namas/backend/database/migrations/2023_08_22_193345_create_purchases_table.php
@@ -18,18 +18,19 @@ return new class extends Migration
             $table->id();
             $table->date('purchase_date');
             $table->string('purchase_place', 255);
-            // $table->float('total_payed', 8, 2)->unsigned(); //must be counted from exact_product or exact_service included in this purchase
-            $table->json('list_of_exact_purchases_ids'); 
-                //json string should consist product or service id, detailed description, purchased ammount, price, discount, etc...
+            $table->string('payment_method', 255);                  //cash, card, bank transfer ...
+            // $table->float('total_payed', 8, 2)->unsigned();      //must be counted for all exact_products or exact_services included in this purchase. Method should be added into Model file.
+            $table->json('list_of_exact_purchases_ids');            //json string should contain exact_products or exact_services ids, ---- detailed description, purchased ammount, price, discount, etc...
 
-                //further strings (documents) require a more detailed analysis of necessity
-            $table->string('purchase_doc', 255)->nullable();
-            $table->string('prepay_doc', 255)->nullable();
-            $table->string('final_payment_doc', 255)->nullable();
-            $table->string('offer_doc', 255)->nullable();
-            $table->string('other_doc', 255)->nullable();
-
-            $table->string('payment_method', 255); //cash, card, bank transfer ...
+            //list of documents of purchase
+            $table->string('offer_doc', 255)->nullable()->comment('išankstinio apmokėjimo sąskaita faktūra');
+            $table->string('purchase_doc', 255)->nullable()->comment('pilnas vienkartinis atsiskaitymas');
+            $table->string('prepay_doc', 255)->nullable()->comment('avansinio mokėjimo pavedimas');
+            $table->string('final_payment_doc', 255)->nullable()->comment('galutinio mokėjimo pavedimas');
+            $table->string('invoice')->nullable()->comment('sąskaita faktūra po pilno atsiskaitymo');   //čia galima pridėti ir garantinius dokumentus arba juos dėti su exact_product / service.
+            $table->string('other_docs', 255)->nullable()->comment('e-parduotuvėje laikomi užsakymo ir/ar apmokėjimo aprašymai');
+            
+            $table->text('coments')->nullable();                     //for comments on payment method or documents.
             
             // $table->integer('warehouse_qty')->default(0);
             // $table->float('price', 12, 2)->unsigned();
