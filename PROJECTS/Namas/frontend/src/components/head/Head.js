dde1330f9a0c2a902eb9ff893fa7cc1dab0a3438
ViliusSenkus <vilius.s@outlook.com>
2023-11-10 19:21:23 +0200

    images moved, links changed

--- PROJECTS/Namas/frontend/src/components/head/Head.js
+++ PROJECTS/Namas/frontend/src/components/head/Head.js
@@ -1,8 +1,5 @@
 import Menu from "../dataPage/menu/Menu";
 import { Link } from "react-router-dom";
-import IconHome from '../../img/icons/home.png';
-import IconNew from '../../img/icons/euro.png';
-import IconTable from '../../img/icons/index.png'
 
 function Head() {
    return (
@@ -15,17 +12,17 @@ function Head() {
             <ul>
                <li>
                   <Link to="/">
-                     <img src={IconHome} alt='home page' />
+                     <img src="img/icons/home.png" alt='home page' />
                   </Link>
                </li>
                <li>
                   <Link to="/entrie">
-                     <img src={IconNew} />
+                     <img src="img/icons/euro.png" alt="new purchase" />
                   </Link>
                </li>
                <li>
                   <Link to="/data">
-                     <img src={IconTable} />
+                     <img src="img/icons/index.png" alt="data sourcess" />
                   </Link>
                </li>
             </ul>
